7e9056a2ca2eb5ebdaeeaf179aa4b555eab66728
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-02-16 11:44:34 -0500

    We don't need remote state.
    Remote state we need is in tf_straycat_svc.

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -3,18 +3,6 @@
 */
 
 /*
-* data
-*/
-data "terraform_remote_state" "aws_vpc" {
-  backend = "s3"
-  config = {
-    bucket  = "${var.domain}-${var.account_id}-terraform"
-    key     = "aws_vpc.tfstate"
-    region  = "${var.aws_region}"
-  }
-}
-
-/*
 * variables
 */
 variable "account_id" {}
